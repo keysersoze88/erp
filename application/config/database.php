f737c1f012eda9e96020ce1da586aa4839501930
keysersoze88 <l498493006@gmail.com>
2021-01-06 18:35:05 +0800

    1

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = '123456';
 $db['default']['database'] = '1999';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'ci_';
